fc7249926e16500159d0a908b439175274dcbfd6
dabin-lee <ldabinn@gmail.com>
2024-08-03 15:56:27 +0900

    detailvue edit

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,13 +1,46 @@
 <template>
 	<div>
-		<h2>게시글 상세</h2>
+		<h2>제목</h2>
+		<p>내용</p>
 		<hr class="my-4" />
-		<p>params: {{ $route.params }}</p>
-		<p>query: {{ $route.query ? $route.query : '없음' }}</p>
-		<p>hash: {{ $route.hash }}</p>
+		<div class="row g-2">
+			<div class="col-auto">
+				<button class="btn btn-outline-dark">이전글</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-dark">다음글</button>
+			</div>
+			<div class="col-auto me-auto"></div>
+			<div class="col-auto">
+				<button class="btn btn-outline-dark" @click="goListPage">목록</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-primary" @click="goEdit">수정</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-danger" @click="goDelete">삭제</button>
+			</div>
+		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goListPage = () => {
+	router.push({
+		name: 'PostList',
+	});
+};const goListPage = () => {
+	router.push({
+		name: 'PostList',
+	});
+};const goListPage = () => {
+	router.push({
+		name: 'PostList',
+	});
+};
+</script>
 
 <style lang="scss" scoped></style>
